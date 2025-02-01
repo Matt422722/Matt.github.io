c8787704096d63fa57c5bb6535e8bdf0085b8c7a
Matthew <matthewhannemann@yahoo.com>
2025-01-31 18:43:07 -0800

    Add files via upload

---
+++ TimeApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "D:\\myRepository\\Portfolio\\currentTime.txt"; 
+        
+        while (true)
+        {
+            // Get the current time and format it
+            string currentTime = DateTime.Now.ToString("h:mm tt");
+
+
+            // Write the current time to the file (overwrites the file)
+            File.WriteAllText(filePath, currentTime);
+
+            // Wait for 1 minute before updating the time again
+            Thread.Sleep(6000);
+        }
+    }
+}
+
+
+
+
+
+
